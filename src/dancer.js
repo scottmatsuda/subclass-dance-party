7d81741c60f08bd2f2fdff7d69c9e88928c9226a
Scott Matsuda <smatsuda2014@gmail.com>
2022-09-12 16:07:02 -0700

    Refactored Dancer and BlinkyDancer to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,34 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
-
-  var dancer = {};
-
+var Dancer = function(top, left, timeBetweenSteps) {
+  // var dancer = {};
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
+  this.$node = $('<span class="dancer"></span>');
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition();
+  this.step();
+  // return dancer;
+};
 
-  return dancer;
+Dancer.prototype.step = function() {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+
+  //Have to call BIND so that 'this' remains as the new object calling the function
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = function() {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: this.top,
+    left: this.left
+  };
+  this.$node.css(styleSettings);
 };
